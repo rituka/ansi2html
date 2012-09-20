d04f345dd1ee00b660c3cd8b668ad23670c42eb7
Jason DeVita <jdevita@arete.com>
2012-09-20 08:01:24 -0700

    better color control
    	modified:   ansi2html.rb

--- ansi2html.rb
+++ ansi2html.rb
@@ -7,9 +7,47 @@
 # editor.rb output.
 #
 # To get the screen print, run:
-# xterm -xrm '*printerCommand: cat > /tmp/xterm_dump' -xrm '*printAttributes: 2'
+# xterm -xrm '*printerCommand: ruby <PATH>/ansi2html.rb > temp.html' -xrm '*printAttributes: 2'
 #
 
+require 'optparse'
+
+# default colors
+$white = "#fff"
+$black = "#000"
+$blue = "#00f"
+$cyan = "#0ff"
+$yellow = "#ff0"
+$green = "#0f0"
+$magenta = "#f0f"
+$red = "#f00"
+$foreground = $black
+$background = $white
+
+
+# process options
+optparse = OptionParser.new{|opts|
+	opts.banner = "Usage: ansi2html [options] file"
+	opts.on('-d','--dark','Use a darker color versions'){
+		$cyan = "#0aa"
+		$yellow = "#aa0"
+		$magenta = "#a0a"
+	}
+	opts.on('-l','--light','Use a lighter color versions'){
+		$blue = "#66f"
+		$green = "#6f6"
+	}
+	opts.on('-b','--background COLOR','Set background color'){|color|
+		$background = color
+	}
+	opts.on('-f','--foreground COLOR','Set foreground color'){|color|
+		$foreground = color
+	}
+}
+optparse.parse!
+
+
+# File or stdin?
 file = ARGV[0]
 if file
 	f = File.open(file)
@@ -17,7 +55,8 @@ else
 	f = STDIN
 end
 
-puts "<pre style=\"border: black 1px solid; display: inline-block\">"
+# process the text
+puts "<pre style=\"border: black 1px solid; display: inline-block; background-color: #{$background}\">"
 f.each_line{|line|
 
 	line.chomp!
@@ -28,32 +67,44 @@ f.each_line{|line|
 	# remove null color at start of line
 	line = line.gsub(/^\e\[0m/,'')
 
-	# process text decoration
-	a = line.partition(/\e\[.*?m/)
+	# process text color/decoration
+	a = line.partition(/\e\[.*?m/)  # split at first escape
 	aline = a[0]
 	while a[1] != ""
+
+		# if reverse gets set, then color turns to background-color
 		cs = "color"
+
+		# interpret each numerical code
 		a[1][2..-2].split(';').each{|code|
 			case code
 				when '0'
-					aline += "<code style=\"color:black; background-color:white; text-decoration:none\">"
+					aline += "<code style=\"color:#{$foreground}; background-color:#{$background}; text-decoration:none\">"
 				when '7'
-					#aline += "<code style=\"background-color:black\">"
 					cs = "background-color"
 				when '36'
-					aline += "<code style=\"#{cs}:blue\">"
+					aline += "<code style=\"#{cs}:#{$cyan}\">"
 				when '34'
-					aline += "<code style=\"#{cs}:cyan\">"
+					aline += "<code style=\"#{cs}:#{$blue}\">"
 				when '33'
-					aline += "<code style=\"#{cs}:orange\">"
+					aline += "<code style=\"#{cs}:#{$yellow}\">"
+				when '31'
+					aline += "<code style=\"#{cs}:#{$red}\">"
+				when '32'
+					aline += "<code style=\"#{cs}:#{$green}\">"
+				when '35'
+					aline += "<code style=\"#{cs}:#{$magenta}\">"
 				when '4'
 					aline += "<code style=\"text-decoration:underline\">"
+				when '3'
+					aline += "<code style=\"text-decoration:blink\">"
 			end
 		}
 		a = a[2].partition(/\e\[.*?m/)
 		aline += a[0]
 	end
 
+	# close tags
 	n = aline.scan("<code").length
 	puts aline + "</code>"*n
 }
